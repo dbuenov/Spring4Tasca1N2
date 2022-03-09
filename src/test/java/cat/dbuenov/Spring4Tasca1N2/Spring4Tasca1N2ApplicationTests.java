ad6b5eb9886582a4b5181ec42bee143f19230f23
Dani Bueno <dbuenov@gmail.com>
2022-03-09 12:37:12 +0100

    N2 amb gradle

---
+++ src/test/java/cat/dbuenov/Spring4Tasca1N2/Spring4Tasca1N2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.dbuenov.Spring4Tasca1N2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4Tasca1N2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
